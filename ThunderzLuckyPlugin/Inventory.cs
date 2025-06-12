f6e8e718a570466e6c3a3c8a1003562bd9b758ae
bananplaysshu <borokbence8@gmail.com>
2025-06-12 15:43:48 +0200

    Initial commit

---
+++ ThunderzLuckyPlugin/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using Il2CppInterop.Runtime.Attributes;
+using Reactor.Utilities.Attributes;
+using System;
+using UnityEngine;
+
+namespace bananplaysshu {
+	[RegisterInIl2Cpp]
+	public class Inventory : MonoBehaviour {
+		[HideFromIl2Cpp]
+		private GameObject inventoryContainer { get; set; }
+
+		public Inventory(IntPtr ptr) : base(ptr) { }
+
+		[HideFromIl2Cpp]
+		public static Inventory Instance { get; set; }
+
+		private void Awake() {
+			Instance = this;
+		}
+
+		private void Start() {
+			inventoryContainer = transform.GetChild(0).gameObject;
+			Debug.Log("Inventory Initialized for 'localPlayer'");
+
+			InventorySystem.Instance.inv.SetActive(false);
+		}
+
+		public void HideIventory() {
+			inventoryContainer.SetActive(false);
+		}
+
+		public void ShowInventory() {
+			inventoryContainer.SetActive(true);
+		}
+	}
+
+	[HarmonyPatch(typeof(HudManager),nameof(HudManager.Update))]
+	public static class HudManPatch {
+		public static void Postfix(HudManager __instance) {
+			__instance.SabotageButton.gameObject.SetActive(false);
+			__instance.ReportButton.gameObject.SetActive(false);
+			__instance.KillButton.GetComponent<SpriteRenderer>().sprite = GurgeUtils.LoadSprite("ThunderzLuckyPlugin.Resources.KillButton.png", 100);
+			__instance.ImpostorVentButton.gameObject.SetActive(false);
+			__instance.KillOverlay.gameObject.SetActive(false);
+		}
+	}
+}
+
---
+++ ThunderzLuckyPlugin/NetherPortal.cs
@@ -0,0 +1,103 @@
+﻿using Il2CppInterop.Runtime.Attributes;
+using Reactor.Utilities.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace bananplaysshu {
+	[RegisterInIl2Cpp]
+	public class NetherPortal : MonoBehaviour{
+		public NetherPortal(IntPtr ptr) : base(ptr) { }
+		public static NetherPortal Instance {  get; private set; }
+
+		#region Fields
+
+		public static float reourcePosY;
+
+		private static float playerPosY = 0;
+		private static float ySubstractFromResourcePos = .75f;
+		private static float checkInputCounter = .25f;
+		private static float checkInputCounterMax = .25f;
+		private static float resourceInteractCooldownCounter = .5f;
+		private static float resourceInteractCooldownCounterMax = .5f;
+
+		public static int interactedCount = 0;
+		public static int interactedCountNeeded = 2;
+		public static int index = 0;
+		public static int index2 = 0;
+		#endregion
+
+		[HideFromIl2Cpp]
+		private List<GameObject> blocks { get; set; }
+
+		private void Awake() {
+			Instance = this;
+
+			blocks = new List<GameObject>();
+			for (int i = 0; i < transform.childCount; i++) {
+				if (transform.GetChild(i).gameObject.name == "Spawn") continue;
+				blocks.Add(transform.GetChild(i).gameObject);
+			}
+			foreach(GameObject go in blocks) {
+				go.SetActive(false);
+			}
+		}
+
+		private void Update() {
+			#region HandleInteractions
+			float distanceNeeded = 4f;
+			float yPosDiff = .7f;
+
+			Vector3 resourcePos = transform.position;
+
+			if (Vector3.Distance(PlayerControl.LocalPlayer.transform.position, resourcePos) < distanceNeeded &&
+
+				(playerPosY - reourcePosY - ySubstractFromResourcePos) <= yPosDiff) {
+
+				if (Hand.Instance == null) return;
+				if (Hand.Instance.IsMining()) {
+					checkInputCounter -= Time.deltaTime;
+					if (checkInputCounter < 0) {
+						checkInputCounter = checkInputCounterMax;
+
+						if (PlayerControl.LocalPlayer.TryGetComponent<InventorySystem>(out InventorySystem inv)) {
+							
+							if (InventoryStorage.Instance.HasInventoryItemInventory(InventoryItemDatabase.Instance.ReturnItemByEnumName(InventoryItemDatabase.InventoryItemsEnum.BucketOfLava)
+							, ref index)) {
+								blocks[index2].SetActive(true);
+								index2++;
+								if (index2 == blocks.Count-1) { blocks[index2].SetActive(true); GetComponent<Collider2D>().enabled = true; index2 = 0; }
+								InventoryStorage.Instance.RemoveInventoryItemFromStorage(InventoryItemDatabase.Instance.ReturnItemByEnumName(InventoryItemDatabase.InventoryItemsEnum.BucketOfLava));
+							}
+						} else {
+							Debug.LogError("Player didn't have an InventorySystem component on 'em. Tragic...");
+						}
+
+						interactedCount = 0;
+					}
+				}
+			}
+		}
+
+		private void OnTriggerEnter2D(Collider2D other) {
+			if (other.transform == PlayerControl.LocalPlayer.transform) {
+				PlayerControl.LocalPlayer.transform.position = NetherPortalBack.Instance.ReturnSpawnPos();
+				foreach (PlayerBodySprite sprite in PlayerControl.LocalPlayer.cosmetics.bodySprites) {
+					sprite.BodySprite.rendererPriority = 5;
+				}
+
+
+			}
+		}
+
+		public Vector3 ReturnSpawnPos() {
+			return transform.GetChild(0).position;
+		}
+	}
+}
+#endregion
