8d863c2e79f3478e2247dd229b77ab56868e153e
bananplaysshu <borokbence8@gmail.com>
2025-06-12 10:57:42 +0200

    Initial commit

---
+++ Buttons/SpeedPotionButton.cs
@@ -0,0 +1,59 @@
+﻿using bananplaysshu;
+using HarmonyLib;
+using Il2CppInterop.Runtime.Attributes;
+using MiraAPI.Hud;
+using MiraAPI.Utilities.Assets;
+using Reactor.Networking.Attributes;
+using System;
+using UnityEngine;
+using static bananplaysshu.ThunderzLuckyPlugin;
+
+namespace bananplaysshu.Buttons {
+
+	[RegisterButton]
+	internal class SpeedPotionButton : CustomActionButton {
+
+		public static bool canUse = true;
+
+		LoadableAsset<Sprite> buttonSprite = new LoadableResourceAsset("ThunderzLuckyPlugin.Resources.SpeedPotion.png");
+
+		public override string Name => "Speed";
+
+		public override float Cooldown => 5f;
+
+		public override float EffectDuration => 0f;
+
+		public override int MaxUses => 0;
+
+		public override LoadableAsset<Sprite> Sprite => buttonSprite;
+		public static bool isSpeeding = false;
+
+
+		public override bool Enabled(RoleBehaviour role) {
+			if (role.TeamType == RoleTeamTypes.Impostor) {
+				Button.buttonLabelText.outlineColor = Color.red;
+			}
+			if (role.TeamType == RoleTeamTypes.Impostor) {
+				return true;
+			}
+			return false;
+		}
+
+		public override ButtonLocation Location => ButtonLocation.BottomLeft;
+
+		protected override void OnClick() {
+			isSpeeding = !isSpeeding;
+			CustomRPC.SpeedPotion(PlayerControl.LocalPlayer, isSpeeding);
+
+		}
+
+		public override bool CanUse() {
+			return canUse;
+		
+		}
+
+		
+	}
+
+	
+}
---
+++ MobsEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bananplaysshu {
+	public enum MobsEnum {
+		Enderman,
+		Blaze,
+		EnderDragon,
+		Piglin,
+	}
+}
